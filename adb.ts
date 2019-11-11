7709dde459244c1ad97664b3a654d052ee4b95d4
arassart <antoinerassart@hotmail.be>
2019-11-11 14:20:53 +0100

    Added comma
    Signed-off-by: arassart <antoinerassart@hotmail.be>

--- adb.ts
+++ adb.ts
@@ -1,6 +1,6 @@
 const totalSteps = (staircase: number[], stepsPerFlight: number) =>
     staircase.reduce((acc, curr) => acc + Math.ceil(curr/stepsPerFlight), 0)
-    + (staircase.length - 1) * 2
+    + (staircase.length - 1) * 2;
 
 console.log(totalSteps([15], 2));
 console.log(totalSteps([15, 15], 2));
